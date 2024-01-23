b75d2ad3a856293ba547252226a305d86a73978e
vinoth <vinoth@payrexx.com>
2024-01-23 13:12:36 +0530

    task/PP-10958: changed spaces to tabs

--- src/Model/PaymentMethod/BobInvoice.php
+++ src/Model/PaymentMethod/BobInvoice.php
@@ -3,11 +3,11 @@
 class WC_Payrexx_Gateway_BobInvoice extends WC_Payrexx_Gateway_Base
 {
 
-    public function __construct()
-    {
-        $this->id = PAYREXX_PM_PREFIX . 'bob-invoice';
-        $this->method_title = __( 'Bob Invoice (Payrexx)', 'wc-payrexx-gateway' );
+	public function __construct()
+	{
+		$this->id = PAYREXX_PM_PREFIX . 'bob-invoice';
+		$this->method_title = __( 'Bob Invoice (Payrexx)', 'wc-payrexx-gateway' );
 
-        parent::__construct();
-    }
+		parent::__construct();
+	}
 }
