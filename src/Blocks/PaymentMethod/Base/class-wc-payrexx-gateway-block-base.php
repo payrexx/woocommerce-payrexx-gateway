d1475cfdb25f41d5204b6e19bab9f64097a9f6e0
vinoth <vinoth@payrexx.com>
2025-03-05 18:08:20 +0530

    bugfix/PP-14507: code improvement

--- src/Blocks/PaymentMethod/Base/class-wc-payrexx-gateway-block-base.php
+++ src/Blocks/PaymentMethod/Base/class-wc-payrexx-gateway-block-base.php
@@ -63,18 +63,18 @@ class WC_Payrexx_Gateway_Block_Base extends AbstractPaymentMethodType {
 	public function get_payment_method_data() {
 		$description = wp_kses_post( $this->settings['description'] );
 		$supports = $this->get_supported_features();
-		if ( 'yes' === $this->settings['subscriptions_enabled'] ) {
+
+		if ( isset($this->settings['subscriptions_enabled'])
+		 	&& 'yes' === $this->settings['subscriptions_enabled']
+			&& SubscriptionHelper::isSubscription( WC()->cart )
+		) {
 			$supports = array_merge(
-				$this->get_supported_features(),
+				$supports,
 				SubscriptionHelper::get_supported_features()
 			);
-
-			// Check if the cart contains a subscription
-			if ( SubscriptionHelper::isSubscription( WC()->cart ) ) {
-				$description .= $this->get_subscription_checkbox();
-			}
+			$description .= $this->get_subscription_checkbox();
 		}
-	
+
 		return [
 			'title'       => $this->get_setting( 'title' ),
 			'description' => $description,
@@ -88,16 +88,15 @@ class WC_Payrexx_Gateway_Block_Base extends AbstractPaymentMethodType {
 	 */
 	private function get_subscription_checkbox():string {
 		$name = esc_attr( 'payrexx-allow-recurring-' . $this->name );
-		$label_text = wp_kses_post( $this->settings['subscriptions_user_desc'] );
+		$label_text = wp_kses_post( $this->settings['subscriptions_user_desc'] ?? '' );
 
 		return sprintf(
 			'<br/>
-			<div class="wc-block-components-checkbox">
-				<label for="%s">
-					<input type="checkbox" class="wc-block-components-checkbox__input" checked name="%s" id="%s" value="1" />
-					%s
-				</label>
-			</div>',
+			<label for="%s">
+				<input type="checkbox" checked="checked" name="%s" id="%s" value="1" />
+				%s
+			</label>
+			',
 			$name, $name, $name, $label_text
 		);
 	}
@@ -107,11 +106,11 @@ class WC_Payrexx_Gateway_Block_Base extends AbstractPaymentMethodType {
 	 */
 	private function get_payment_logo():string {
 		$pm = str_replace(PAYREXX_PM_PREFIX, '', $this->name);
-		
+
 		$pm = ( 'payrexx' == $pm ? '' : $pm);
 		$icon = '';
 		if ( $pm ) {
-			$src  = WC_HTTPS::force_https_url( 
+			$src  = WC_HTTPS::force_https_url(
 				plugins_url( 'includes/cardicons/card_' . $pm . '.svg', PAYREXX_MAIN_FILE )
 			);
 			return '<img src="' . $src . '" alt="' . $pm . '" id="payrexx-' . $pm . '"/>';
@@ -122,16 +121,12 @@ class WC_Payrexx_Gateway_Block_Base extends AbstractPaymentMethodType {
 				$logos = SubscriptionHelper::isSubscription( WC()->cart ) ? $subscription_logos : $logos;
 				$icon  = '';
 				foreach ( $logos as $logo ) {
-					$src   = WC_HTTPS::force_https_url( 
-						plugins_url( 'includes/cardicons/card_' . $logo . '.svg', PAYREXX_MAIN_FILE ) 
+					$src   = WC_HTTPS::force_https_url(
+						plugins_url( 'includes/cardicons/card_' . $logo . '.svg', PAYREXX_MAIN_FILE )
 					);
 					$icon .= '<img src="' . $src . '" alt="' . $logo . '" id="payrexx-' . $logo . '"/>';
 				}
 			}
-		}
-		if ($pm === 'post-finance-pay') {
-			// echo $icon;
-			// exit();
 		}
 		return $icon;
 	}
