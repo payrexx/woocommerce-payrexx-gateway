9013d5fe4c5b616562519424883a1da3256b3b29
vinoth <vinoth@payrexx.com>
2025-05-19 19:08:08 +0530

    task/PP-15257: change Cembrapay to CembraPay

--- src/Blocks/PaymentMethod/class-wc-payrexx-gateway-cembrapay-block.php
+++ src/Blocks/PaymentMethod/class-wc-payrexx-gateway-cembrapay-block.php
@@ -6,9 +6,9 @@
 use Automattic\WooCommerce\Blocks\Payments\Integrations\AbstractPaymentMethodType;
 
 /**
- * WC_Payrexx_Gateway_Cembrapay_Block
+ * WC_Payrexx_Gateway_CembraPay_Block
  */
-class WC_Payrexx_Gateway_Cembrapay_Block extends WC_Payrexx_Gateway_Block_Base {
+class WC_Payrexx_Gateway_CembraPay_Block extends WC_Payrexx_Gateway_Block_Base {
 	/**
 	 * Payment method name
 	 *
