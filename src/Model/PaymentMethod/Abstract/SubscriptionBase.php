eff583ccdbc26741895b57d6d8163dc9d5e9c811
Morris Schneider <morris.schneider@payrexx.com>
2026-08-17 16:06:19 +0200

    bugfix/PP-20452: cast Payrexx gateway reference to string so numeric order IDs survive the JSON request body

--- src/Model/PaymentMethod/Abstract/SubscriptionBase.php
+++ src/Model/PaymentMethod/Abstract/SubscriptionBase.php
@@ -142,7 +142,7 @@ abstract class WC_Payrexx_Gateway_SubscriptionBase extends WC_Payrexx_Gateway_Ba
 		}
 		$total_amount                 = floatval( $order->get_total( 'edit' ) );
 		$prefix                       = get_option( PAYREXX_CONFIGS_PREFIX . 'prefix' );
-		$data['reference']            = $prefix ? $prefix . '_' . $order_id : $order_id;
+		$data['reference']            = (string) ( $prefix ? $prefix . '_' . $order_id : $order_id );
 		$data['success_redirect_url'] = $this->get_return_url( $order );
 		$data['cancel_redirect_url']  = PaymentHelper::getCancelUrl( $order );
 		$data['language']             = $this->get_gateway_lang();
